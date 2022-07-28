cd99442d5f0a1c18e4e7846952bfe9328e8a9745
Christian Holland <christian.holland@inventor-e.co.uk>
2022-07-28 21:07:28 +0100

    Vehicle tax is calculated and set

---
+++ app/models/concerns/tax_rates.rb
@@ -0,0 +1,42 @@
+module TaxRates
+  extend ActiveSupport::Concern
+
+  def get_tax_rate(vehicle)
+    return 0 if vehicle.stock_type_used?
+
+    tax = 55
+
+    if vehicle.commercial?
+      tax += vehicle.carbon_based? ? 290 : 0
+    else
+      case vehicle.co2
+      when 0
+        return tax
+      when 1..50
+        tax += vehicle.carbon_based? ? 10 : 0
+      when 51..75
+        tax += vehicle.carbon_based? ? 25 : 15
+      when 76..90
+        tax += vehicle.carbon_based? ? 120 : 110
+      when 91..100
+        tax += vehicle.carbon_based? ? 150 : 140
+      when 101..110
+        tax += vehicle.carbon_based? ? 170 : 160
+      when 111..130
+        tax += vehicle.carbon_based? ? 190 : 180
+      when 131..150
+        tax += vehicle.carbon_based? ? 230 : 220
+      when 151..170
+        tax += vehicle.carbon_based? ? 585 : 575
+      when 171..190
+        tax += vehicle.carbon_based? ? 945 : 935
+      when 191..225
+        tax += vehicle.carbon_based? ? 1420 : 1410
+      else
+        tax += vehicle.carbon_based? ? 2365 : 2355
+      end
+    end
+
+    tax
+  end
+end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,49 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id                 :integer          not null, primary key
+#  co2                :integer
+#  commercial_vehicle :boolean
+#  fuel_type          :string
+#  stock_type         :string
+#  vehicle_tax        :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 class Vehicle < ApplicationRecord
+  include TaxRates
+
   enum stock_type: { new: 'new', used: 'used'}, _prefix: true
-  enum fuel_type: { electric: 'electric', hybrid: 'hybrid', pertrol: 'petrol', diesel: 'diesel'}
+  enum fuel_type: { electric: 'electric', hybrid: 'hybrid', petrol: 'petrol', diesel: 'diesel'}
 
   validates :stock_type, presence: true
+  validates :commercial_vehicle, inclusion: { in: [ true, false ] }
+  validates :co2, presence: true
+  validates :fuel_type, presence: true
+
+  before_save :calculate_tax
+
+  def carbon_based?
+    petrol? || diesel?
+  end
+
+  def commercial?
+    commercial_vehicle
+  end
+
+  # I normally have the permitted params in the controller, but as I'm not writing
+  # the view/controllers myself, I'll put the params in here for ease of use for the
+  # dev that's doing the VC work.
+  def self.permitted_params
+    [:co2, :commercial_vehicle, :fuel_type, :stock_type, :vehicle_tax]
+  end
+
+  private
+
+  def calculate_tax
+    self.vehicle_tax = get_tax_rate(self)
+  end
 
   # ...
   # 100s of lines of code deleted, as they are not relevant to this task
