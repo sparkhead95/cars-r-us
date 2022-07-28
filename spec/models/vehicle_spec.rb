ea065e88806f47d473eb6cb73a919eb1948af76d
Christian Holland <christian.holland@inventor-e.co.uk>
2022-07-28 21:10:20 +0100

    Revert "Vehicle tax is calculated and set"
    This reverts commit cd99442d5f0a1c18e4e7846952bfe9328e8a9745.

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,16 +1,3 @@
-# == Schema Information
-#
-# Table name: vehicles
-#
-#  id                 :integer          not null, primary key
-#  co2                :integer
-#  commercial_vehicle :boolean
-#  fuel_type          :string
-#  stock_type         :string
-#  vehicle_tax        :integer
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
