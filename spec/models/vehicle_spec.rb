72e4530906262295f3164b6caa93d9258305db33
Christian Holland <christian.holland@inventor-e.co.uk>
2022-07-28 21:15:02 +0100

    Vehicle tax is calculated and set

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id                 :integer          not null, primary key
+#  co2                :integer
+#  commercial_vehicle :boolean
+#  fuel_type          :string
+#  stock_type         :string
+#  vehicle_tax        :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
